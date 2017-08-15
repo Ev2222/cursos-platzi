14cd1ae8b7a48f83e93d7d8c7d60b3a58c04b6fe
Eva Reyes B <rb.eva22@gmail.com>
2017-08-15 12:26:20 -0500

    Actualización de variable

--- programacion basica/teclado/eventosTeclado.js
+++ programacion basica/teclado/eventosTeclado.js
@@ -6,44 +6,55 @@ var teclas = {
     RIGHT : 39,
     DOWN : 40
 };
+
 console.log(teclas);
 
 var d = document.getElementById("areaDeDibujo");
-var lienzo = d.getContext("2d");
+var papel = d.getContext("2d");
 
 function dibujarLinea(color, xInicial, yInicial, xFinal, yFinal)
 {
-    lienzo.beginPath();
-    lienzo.strokeStyle = color;
-    lienzo.moveTo(xInicial, yInicial);
-    lienzo.lineTo(xFinal, yFinal);
-    lienzo.stroke();
-    lienzo.closePath();
+    papel.beginPath();
+    papel.strokeStyle = color;
+    papel.lineWidth = 3;
+    papel.moveTo(xInicial, yInicial);
+    papel.lineTo(xFinal, yFinal);
+    papel.stroke();
+    papel.closePath();
 }
 
-// posicionInicial
-//nuevaPosicion
-// function posicion(){
-var centroHorizontal = (d.width)/2;
-var centroVertical = (d.height)/2    
-// }
-function dibujarTeclado(evento){
+var puntoInicioX = (d.width)/2;
+var puntoInicioY = (d.height)/2  
+//punto inicial
+dibujarLinea("red", puntoInicioX-1, puntoInicioY-1, puntoInicioX+1, puntoInicioY+1);
+
+
+function dibujarTeclado(evento)
+{
+    var colorcito =  "Cyan";
+    var movimiento = 10;
+
     switch(evento.keyCode)
     {
         case teclas.LEFT:
-        console.log("Izquierda");
-        
-        dibujarLinea("cyan", centroHorizontal, centroVertical, centroHorizontal -100, centroVertical);
-
+            console.log("Izquierda");
+            dibujarLinea("cyan", puntoInicioX, puntoInicioY, puntoInicioX - movimiento, puntoInicioY);
+            puntoInicioX = puntoInicioX - movimiento;
         break;
         case teclas.UP:
-        console.log("Arriba");
+            console.log("Arriba");
+            dibujarLinea("cyan", puntoInicioX, puntoInicioY, puntoInicioX , puntoInicioY - movimiento);
+            puntoInicioY = puntoInicioY - movimiento;
         break;
         case teclas.RIGHT:
-        console.log("Derecha");
+            console.log("Derecha");
+            dibujarLinea("cyan", puntoInicioX, puntoInicioY, puntoInicioX + movimiento, puntoInicioY);
+            puntoInicioX = puntoInicioX + movimiento;
         break;
         case teclas.DOWN:
         console.log("Abajo");
+            dibujarLinea("cyan", puntoInicioX, puntoInicioY, puntoInicioX , puntoInicioY + movimiento);
+            puntoInicioY = puntoInicioY + movimiento;
         break;
         default:
         console.log("Otra tecla");
